369505b29dc7acb5d7931ecc7351c85bf42f799e
LeaderbotX400 <eric@singerfamily.ca>
2022-02-08 19:58:43 -0700

    change jQuery to be on google

---
+++ .pokedex/index.js
@@ -0,0 +1,36 @@
+let url = 'http://localhost:5500/pokemon.json';
+let dex;
+$.ajax({
+  type: 'GET',
+  url: url,
+  dataType: 'json',
+  success: function(data) { dex = data;},
+  async: false
+});
+
+console.log(dex)
+
+//generate new item
+async function generate(name, num) {
+  let list = document.getElementById("list");
+  let listItem = document.createElement("li");
+
+  let pokemonName = document.createElement("h3");
+  pokemonName.innerHTML = `Name: ${name}`;
+
+  let number = document.createElement("h4");
+  number.innerHTML = `Entry Number: ${num}`;
+
+  let img = document.createElement("img");
+  img.src = "";
+
+  list.appendChild(listItem);
+  list.appendChild(pokemonName);
+  list.appendChild(number);
+  list.appendChild(img);
+}
+
+//loop through dex
+for (let i = 0; i < dex.length; i++) {
+  generate(dex[i].name, dex[i].national);
+}
