d3a857ec2b774228e83f2694ac9537e0912cc2e4
ericsinger <erics118@educbe.ca>
2022-03-02 13:34:08 -0700

    revert API key

--- projects/fetch/weather/app.js
+++ projects/fetch/weather/app.js
@@ -1,3 +1,5 @@
+// api key : 82005d27a116c2880c8f0fcb866998a0
+
 // SELECT ELEMENTS
 const iconElement = document.querySelector(".weather-icon");
 const tempElement = document.querySelector(".temperature-value p");
@@ -15,7 +17,7 @@ weather.temperature = {
 // APP CONSTS AND VARS
 const KELVIN = 273;
 // API KEY
-const key = "2a07bc29d48aa9aef9088e9160b8f084";
+const key = "82005d27a116c2880c8f0fcb866998a0";
 
 // CHECK IF BROWSER SUPPORTS GEOLOCATION
 if ("geolocation" in navigator) {
@@ -41,6 +43,7 @@ function showError(error) {
 
 // GET WEATHER FROM API PROVIDER
 function getWeather(latitude, longitude) {
+  console.log(`${latitude} ${longitude}`);
   let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
 
   fetch(api)
