6fe1c9235119a3515b7d1b009692d5740b053be2
BuildTools <unconfigured@null.spigotmc.org>
2022-03-18 14:06:12 -0600

    init

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
+<navbar></navbar>
 </template>
 
 <style>
 @import "./assets/base.css";
-
-#app {
-  max-width: 1280px;
-  margin: 0 auto;
-  padding: 2rem;
-
-  font-weight: normal;
-}
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,99 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "@/App.vue";
+
+const routes = [
+  {
+    path: "/Home", // this is the path that will be used in the url
+    name: "Home", // this is the name of the route
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      // this is the component that will be loaded
+      import(/* webpackChunkName: "about" */ "../views/Home.vue"), // this is the path to the component being imported
+  },
+  //404 error on root url fix v2
+  {
+    path: "/", // this is the path that will be used in the url
+    name: "Home", // this is the name of the route
+    component: () =>
+      // this is the component that will be loaded
+      import(/* webpackChunkName: "about" */ "../views/Home.vue"), // this is the path to the component being imported
+  },
+  {
+    path: "/dragon", // this is the path that will be used in the url
+    name: "Dragon", // this is the name of the route
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      // this is the component that will be loaded
+      import(
+        /* webpackChunkName: "about" */ "../views/games/dragon/dragon.vue"
+      ), // this is the path to the component being imported
+  },
+  // {
+  //     path: '/shop',
+  //     name: 'Shop',
+  //     component: () =>
+  //         import(/* webpackChunkName: "shop" */ '../views/Shop.vue'),
+  // },
+  // {
+  //     path: '/login',
+  //     name: 'Login',
+  //     component: () =>
+  //         import(/* webpackChunkName: "login" */ '../views/Login.vue'),
+  // },
+  // {
+  //     path: '/cart',
+  //     name: 'Cart',
+  //     component: () =>
+  //         import(/* webpackChunkName: "contact" */ '../views/Cart.vue'),
+  // },
+  // //add in contact page
+  // /*{
+  //     path: '/contact',
+  //     name: 'Contact',
+  //     component: () =>
+  //         import(/* webpackChunkName: "contact" */ //'../views/Contact.vue'),
+  // //},
+  // {
+  //     path: '/build',
+  //     name: 'Build',
+  //     component: () =>
+  //         import(/* webpackChunkName: "build" */ '../views/Build.vue'),
+  // },
+
+  // {
+  //     path: '/sign-up',
+  //     name: 'Sign-Up',
+  //     component: () =>
+  //         import(/* webpackChunkName: "build" */ '../views/Sign-Up.vue'),
+  // },
+  // /*{
+  //     path: '/signin',
+  //     name: 'Signin',
+  //     component: () =>
+  //         import(/* webpackChunkName: "signup" */ // '../views/Signin.vue'),
+  // //},
+  // {
+  //     path: '/offline',
+  //     name: 'offline',
+  //     component: () =>
+  //         import(/* webpackChunkName: "offline" */ '../views/offline.vue'),
+  // },
+  // {
+  //     path: '/:pathMatch(.*)*',
+  //     name: 'NotFound',
+  //     component: () =>
+  //         import(
+  //             /* webpackChunkName: "notfound" */ '../views/page404NotFound.vue'
+  //         ),
+  // },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home,
-    },
-  ],
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
