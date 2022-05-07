e46ea964049e27b8bbdd637610307e0c6158c126
BuildTools <unconfigured@null.spigotmc.org>
2022-05-06 20:39:12 -0600

    add mobile navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,46 @@
+<template>
+  <Navbar v-if="!mobile" />
+  <NavbarMobile v-else />
+  <div id="firebaseui-auth-container"></div>
+  <router-view :key="$route.fullPath" />
+</template>
+
 <script>
 import Navbar from "./components/navbar.vue";
+import NavbarMobile from "./components/navbar-mobile.vue";
 export default {
-  components: { Navbar },
+  components: { Navbar, NavbarMobile },
+  data() {
+    return {
+      mobile: false,
+      width: null,
+    };
+  },
+  methods: {
+    checkIfMobile() {
+      if (this.width < 600) {
+        this.mobile = true;
+      } else {
+        this.mobile = false;
+      }
+    },
+    getScreenWidth() {
+      return (
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth
+      );
+    },
+    handleResize() {
+      this.width = this.getScreenWidth();
+      this.checkIfMobile();
+    },
+  },
+  mounted() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.handleResize);
+  },
 };
 </script>
-<template>
-  <Navbar />
-  <div id="firebaseui-auth-container"></div>
-  <router-view />
-</template>
