fd212be59c4bdef71b51c32e5d32155904e9f2e7
BuildTools <unconfigured@null.spigotmc.org>
2022-08-21 15:37:21 -0600

    make router work

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,70 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { useFavicon, useTitle } from "@vueuse/core";
+
+interface meta {
+  favicon: string;
+  background: {
+    type: string;
+    src: string;
+  };
+}
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
+      name: "Home",
       component: () => import("../views/Home.vue"),
+      meta: {
+        favicon: "default",
+        background: {
+          type: "image",
+          src: "default",
+        },
+      },
     },
     {
       path: "/login",
-      name: "login",
+      name: "Login",
       component: () => import("../views/Login.vue"),
+      meta: {
+        favicon: "default",
+      },
+    },
+    {
+      path: "/games/dragon",
+      name: "Dragon",
+      component: () => import("../views/projects/Dragon.vue"),
+      meta: {
+        favicon: "dragon",
+        background: {
+          type: "image",
+          src: "default",
+        },
+      },
+    },
+    {
+      path: "/apis/pokedex",
+      name: "Pokedex",
+      component: () => import("../views/projects/Pokedex.vue"),
+      meta: {
+        favicon: "pokemon",
+        background: {
+          type: "image",
+          src: "default",
+        },
+      },
     },
   ],
+});
+router.afterEach((to, from) => {
+  const title = useTitle();
+  const favicon = useFavicon();
+  title.value = to.name as string;
+
+  // @ts-expect-error
+  favicon.value = `/favicons/${to.meta.favicon}.ico` as string;
 });
 
 export default router;
